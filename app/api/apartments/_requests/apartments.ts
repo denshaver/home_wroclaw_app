aae4d3caa1d03cf3f81515837a112a859b74d150
Denis Vishchuk <d.vishchuk@wenite.de>
2025-11-10 08:34:33 +0100

    fix main: revalidation on fetch

--- app/api/apartments/_requests/apartments.ts
+++ app/api/apartments/_requests/apartments.ts
@@ -2,7 +2,9 @@ import { IApartment } from "@/app/api/apartments/_types/apartments";
 import { baseUrl } from "../../utils";
 
 export default async function getApartments() {
-  const res = await fetch(`${baseUrl}/apartments`);
+  const res = await fetch(`${baseUrl}/apartments`, {
+    next: { revalidate: 20 },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch apartments");
